9730284e6e96bde1f0f436f7c452c6a325abfbbc
Godzilla <godzilla.toonba@gmail.com>
2022-07-06 13:50:01 +0200

    First commit of my CV web Page project
    I strated a website to display my CV an train my self, I had write a html, CSS, JS files + some picture that I commited for the first time here

---
+++ afficher_masquer.js
@@ -0,0 +1,33 @@
+let linkFormation = document.getElementById("linkFormation");
+let formation = document.getElementById("formation");
+let linkExperience = document.getElementById("linkExperience");
+let experience = document.getElementById("experience");
+let linkCompetence = document.getElementById("linkCompetence");
+let competence = document.getElementById("competence");
+let miseEnForme = document.getElementById("miseEnForme");
+let croix = document.getElementById("croix");
+
+function togg1(){
+  miseEnForme.style.visibility = "hidden";
+  formation.style.display = "block";
+};
+linkFormation.onclick = togg1;
+
+function togg2(){
+  miseEnForme.style.visibility = "hidden";
+  experience.style.display = "block";
+};
+linkExperience.onclick = togg2;
+
+function togg3(){
+  miseEnForme.style.visibility = "hidden";
+  competence.style.display = "block";
+};
+
+linkCompetence.onclick = togg3;
+
+function close1() {
+  miseEnForme.style.visibility="visible"
+  formation.style.display = "none";
+}
+croix.onclick = close1
