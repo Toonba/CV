be8e6f7dc3bf7b39978212d6a3acb361a01dd448
Godzilla <godzilla.toonba@gmail.com>
2022-07-08 14:15:08 +0200

    Script modifié afficher article marche

--- afficher_masquer.js
+++ afficher_masquer.js
@@ -1,3 +1,27 @@
+//Afficher l'article formation quand on click sur le lien formation du nav 
+document.getElementById('linkFormation').addEventListener('click', (e) => {
+    document.getElementById("formation").style.display = "block";
+});
+
+//Afficher l'article Experience  quand on click sur le lien Experience  du nav 
+document.getElementById('linkExperience').addEventListener('click', (e) => {
+    document.getElementById("experience").style.display = "block";
+});
+
+//Afficher l'article Compétence quand on click sur le lien Compétence du nav 
+document.getElementById('linkCompetence').addEventListener('click', (e) => {
+    document.getElementById("competence").style.display = "block";
+});
+
+//Afficher l'article contacter quand on click sur le lien contacter du nav 
+document.getElementById('linkContacter').addEventListener('click', (e) => {
+    document.getElementById("contacter").style.display = "block";
+});
+
+//Fermer l'article avec la croix
+document.getElementsById('croix1').addEventListener('click', (e) => {
+    document.getElementById('formation').style.display = "none";
+});
 
 
 /* I have to work on this JS script to be able to make appears or disapears the article on my webpage*/
