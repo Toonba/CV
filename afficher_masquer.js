c5fac1eb54c4d9c1d6ff807835a9fb0df3b93bb9
Godzilla <godzilla.toonba@gmail.com>
2022-07-08 13:49:05 +0200

    Remise en ordre du fichier CSS

--- afficher_masquer.js
+++ afficher_masquer.js
@@ -1,35 +1,6 @@
-let linkFormation = document.getElementById("linkFormation");
-let formation = document.getElementById("formation");
-let linkExperience = document.getElementById("linkExperience");
-let experience = document.getElementById("experience");
-let linkCompetence = document.getElementById("linkCompetence");
-let competence = document.getElementById("competence");
-let miseEnForme = document.getElementById("miseEnForme");
-let croix = document.getElementById("croix");
 
-function togg1(){
-  miseEnForme.style.visibility = "hidden";
-  formation.style.display = "block";
-};
-linkFormation.onclick = togg1;
-
-function togg2(){
-  miseEnForme.style.visibility = "hidden";
-  experience.style.display = "block";
-};
-linkExperience.onclick = togg2;
-
-function togg3(){
-  miseEnForme.style.visibility = "hidden";
-  competence.style.display = "block";
-};
-
-linkCompetence.onclick = togg3;
-
-function close1() {
-  miseEnForme.style.visibility="visible"
-  formation.style.display = "none";
-}
-croix.onclick = close1
 
 /* I have to work on this JS script to be able to make appears or disapears the article on my webpage*/
+/* Apperement .toggle permet de supprimer, ajouter une class à une balise, est ce que je pourrais pas m'en servir pour 
+ajouter une classe à mes articles que je veux faire apparaitre sur mes liens genre 
+si je clic j'ajoute la classe .active qui aura des paramètre en CSS qui permettrons de la faire apparaitre comme je veux*/
